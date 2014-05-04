db7167c8afcd62c26511b754af1b0c83415e1adc
Sparty93 <robertmarchiori@me.com>
2014-05-04 21:44:15 +1000

    Initial Upload

--- Platform Chicken/MyScene.m
+++ Platform Chicken/MyScene.m
@@ -14,18 +14,47 @@
     if (self = [super initWithSize:size]) {
         /* Setup your scene here */
         
-        self.backgroundColor = [SKColor colorWithRed:0.15 green:0.15 blue:0.3 alpha:1.0];
+        self.physicsWorld.gravity = CGVectorMake(0.0, -2.0f);
         
-        SKLabelNode *myLabel = [SKLabelNode labelNodeWithFontNamed:@"Chalkduster"];
+        SKSpriteNode *landscape = [SKSpriteNode spriteNodeWithImageNamed:@"Background-L1"];
+        landscape.position = CGPointMake(CGRectGetMidX(self.frame), CGRectGetMidY(self.frame));
+        landscape.zPosition = -100;
+        landscape.xScale = 0.75;
+        landscape.yScale = 0.75;
         
-        myLabel.text = @"Hello, World!";
-        myLabel.fontSize = 30;
-        myLabel.position = CGPointMake(CGRectGetMidX(self.frame),
-                                       CGRectGetMidY(self.frame));
+        SKSpriteNode *platform = [SKSpriteNode spriteNodeWithImageNamed:@"Platform-L1"];
+        platform.position = CGPointMake(CGRectGetMidX(self.frame), 375);
+        platform.zPosition = -99;
+        platform.xScale = 0.75;
+        platform.yScale = 0.75;
+        platform.physicsBody = [SKPhysicsBody bodyWithRectangleOfSize:CGSizeMake(400, 220)];
+        platform.physicsBody.dynamic = NO;
         
-        [self addChild:myLabel];
+        [self addChild:landscape];
+        [self addChild:platform];
+        
+        
+        for (int i = 1; i<6; i++) {
+             [self createStuffToThrow];
+        }
+       
+    
     }
     return self;
+}
+
+-(void)createStuffToThrow {
+    
+    SKSpriteNode *Stuff = [SKSpriteNode spriteNodeWithImageNamed:@"Saucepan"];
+    Stuff.position = CGPointMake(CGRectGetMidX(self.frame), CGRectGetMaxY(self.frame));
+    Stuff.physicsBody = [SKPhysicsBody bodyWithRectangleOfSize:CGSizeMake(40,40)];
+    Stuff.zPosition = 100;
+    Stuff.physicsBody.mass = -1;
+    Stuff.xScale = 0.2;
+    Stuff.yScale = 0.2;
+    
+    [self addChild:Stuff];
+    
 }
 
 -(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
@@ -34,9 +63,13 @@
     for (UITouch *touch in touches) {
         CGPoint location = [touch locationInNode:self];
         
-        SKSpriteNode *sprite = [SKSpriteNode spriteNodeWithImageNamed:@"Spaceship"];
+        SKSpriteNode *sprite = [SKSpriteNode spriteNodeWithImageNamed:@"Mutant-Chicken"];
         
         sprite.position = location;
+        sprite.physicsBody = [SKPhysicsBody bodyWithCircleOfRadius:50];
+        sprite.physicsBody.mass = 1000000;
+        sprite.xScale = .75;
+        sprite.yScale = .75;
         
         SKAction *action = [SKAction rotateByAngle:M_PI duration:1];
         
